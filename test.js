70d9fc6d8a23df8816a3089275e5a09d56efe02f
Alex Grafen <alex.grafen@gmail.com>
2022-06-08 14:55:42 +0100

    more work on fetching

--- test.js
+++ test.js
@@ -5,22 +5,7 @@ function randomRace() {
 
     console.log(randomYear)
 
-    fetch(`http://ergast.com/api/f1/${randomYear}.json`)
-    .then((response) => {
-        if (!response.ok) {
-            const error = new Error(response.status);
-            throw error;
-        }
-        else {
-            return response.json();
-        }
-    })
-    .then((data) => {
-        let racesLength = data["MRData"]["RaceTable"]["Races"].length;
-        return Math.floor(Math.random() * (racesLength))
-    })
-    .then((round) => {
-        fetch(`https://ergast.com/api/f1/${randomYear}/${round}/results.json`)
+    return fetch(`http://ergast.com/api/f1/${randomYear}.json`)
         .then((response) => {
             if (!response.ok) {
                 const error = new Error(response.status);
@@ -31,9 +16,43 @@ function randomRace() {
             }
         })
         .then((data) => {
-            console.log(data)
+            let racesLength = data["MRData"]["RaceTable"]["Races"].length;
+            return Math.floor(Math.random() * (racesLength))
         })
+        .then((round) => {
+            return fetch(`https://ergast.com/api/f1/${randomYear}/${round}/results.json`)
+                .then((response) => {
+                    if (!response.ok) {
+                        const error = new Error(response.status);
+                        throw error;
+                    }
+                    else {
+                        return response.json();
+                    }
+                })
+                .then((data) => {
+                    console.log(data);
+                    return data;
+                })
+        })
+}
+
+function getDrivers() {
+    randomRace().then((data) => {
+        const raceInfo = data["MRData"]["RaceTable"]["Races"][0]
+        console.log(raceInfo.season);
+        console.log(raceInfo.raceName);
+        // const {'a', 'b', 'c', 'd'} = raceInfo["Results"];
+        // console.log(0)
+        // .map((driver) => {
+        //     console.log(driver["Constructor"]["name"]);
+        //     console.log(driver["Driver"]["givenName"] + " " + driver["Driver"]["familyName"]);
+        //     console.log(driver["position"]);
+        //     console.log(driver["Time"]["millis"]);
+        //     console.log(driver["Time"]["time"]);
+        // })
     })
 }
 
 randomRace()
+getDrivers()
