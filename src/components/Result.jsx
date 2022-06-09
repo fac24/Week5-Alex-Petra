f8c3855210a8c8f1020e319d5d03fe759ca437b7
Petra <asomeonelse@gmail.com>
2022-06-09 14:04:43 +0100

    fixing error

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 function Result(props) {
+    console.log(props.drivers)
     if (!props.drivers) return <div>Loading results...</div>;
     return (
         <div>
             <h2>Results</h2>
             <div>{props.drivers.map((driver) =>
-                driver.position == '1' ? <p key={driver.drivername + 'result'}>{driver.drivername} came first!</p> : <p></p>
+                driver.position == '1' ? <p key={driver.position}>{driver.driverName} came first!</p> : <p key={driver.position}></p>
             )}</div>
         </div>
     )
