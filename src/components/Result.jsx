43ab5ab68c04fb79a67b5593d8336c51b15d69cd
Alex Grafen <alex.grafen@gmail.com>
2022-06-09 13:05:16 +0100

    basic return of result (incomplete)

---
+++ src/components/Result.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Result(props) {
+    if (!props.drivers) return <div>Loading results...</div>;
+    return (
+        <div>
+            <h2>Results</h2>
+            <div>{props.drivers.map((driver) =>
+                driver.position == '1' ? <p key={driver.drivername + 'result'}>{driver.drivername} came first!</p> : <p></p>
+            )}</div>
+        </div>
+    )
+}
+
+export default Result
