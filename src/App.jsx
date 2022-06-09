9929169480425c5d2657c79a3a406597f80f86de
Alex Grafen <alex.grafen@gmail.com>
2022-06-09 13:03:38 +0100

    link in more jsx files and pass properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import logo from './logo.svg'
+// import logo from './logo.svg'
 import './App.css'
-import TotalRandom from "./components/TotalRandom";
-import Bet from "./components/Bet";
+import TotalRandom from "./components/TotalRandom.jsx";
+import Bet from "./components/Bet.jsx";
+import Capital from "./components/Capital.jsx";
+import Result from "./components/Result.jsx";
 
 function App() {
-  const [race, setRace] =  React.useState(null)
-  const [coin, setCoin] = React.useState(100)
+  const [race, setRace] = React.useState(null);
+  const [coin, setCoin] = React.useState(100);
+  const [drivers, setDrivers] = React.useState();
+  const [P1_bet, setP1_bet] = React.useState(0);
+  const [P2_bet, setP2_bet] = React.useState(0);
+  const [P3_bet, setP3_bet] = React.useState(0);
+  const [P4_bet, setP4_bet] = React.useState(0);
 
   return (
     <div className="App">
@@ -14,15 +21,37 @@ function App() {
         <h1>Past & Curious</h1>
       </header>
       <main>
-          <TotalRandom race={race} setRace={setRace} />
-           {/* <RandomInYear /> */}
-        <form>
-          <Bet race={race} coin={coin} setCoin={setCoin}/>
-        </form>
-       {/* <section>
-          <Race /> {/* button that starts the race, animation 
-          <Result />
-        </section> */}
+        <TotalRandom race={race} setRace={setRace} />
+        {/* <RandomInYear /> */}
+        <Capital coin={coin} />
+        <Bet
+          race={race}
+          coin={coin}
+          setCoin={setCoin}
+          drivers={drivers}
+          setDrivers={setDrivers}
+          P1_bet={P1_bet}
+          setP1_bet={setP1_bet}
+          P2_bet={P2_bet}
+          setP2_bet={setP2_bet}
+          P3_bet={P3_bet}
+          setP3_bet={setP3_bet}
+          P4_bet={P4_bet}
+          setP4_bet={setP4_bet} />
+        <section>
+          {/* <Race /> {/* button that starts the race, animation */}
+          <Result
+            race={race}
+            coin={coin}
+            setCoin={setCoin}
+            drivers={drivers}
+            setDrivers={setDrivers}
+            P1_bet={P1_bet}
+            P2_bet={P2_bet}
+            P3_bet={P3_bet}
+            P4_bet={P4_bet}
+          />
+        </section>
       </main>
     </div>
   )
